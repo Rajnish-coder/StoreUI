39bcf451c18aa998cbecdf458cfc067607063984
Rajnish <rshonkhia65@gmail.com>
2025-09-26 00:16:57 +0530

    test functionality added

---
+++ src/sum.test.js
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+function sum(a, b) {
+  return a + b;
+}
+
+describe("sum", () => {
+  it("should sum two numbers", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
