3c1969bacf0c4ca7d187fca0b0cf693136fb416b
Rajnish <rshonkhia65@gmail.com>
2025-09-26 00:03:32 +0530

    tailwind css added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import ProductListings from "./ProductListings";
 function Home() {
   return (
     <div className="home-container">
+      <h1 className="text-3xl font-bold underline">TailWind CSS</h1>
       <PageHeading title="Explore Crazy Stickers!">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ducimus
         aut laborum aliquam unde accusantium voluptates ipsam omnis reiciendis,
