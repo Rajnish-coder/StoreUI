e98debf2f87ae70376fde7ea2e1b9f39dcae26b1
Rajnish <rshonkhia65@gmail.com>
2025-10-05 23:53:25 +0530

    axios added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,48 @@
 import PageHeading from "./PageHeading";
-import products from "../data/products";
 import ProductListings from "./ProductListings";
+import apiClient from "../api/apiClient";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
+      const response = await apiClient.get("/product");
+      setProducts(response.data);
+    } catch (error) {
+      setError(
+        error.response?.data?.error ||
+          "Error in loading data! Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-xl font-semibold">Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-2xl text-red-500">Error: {error}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Crazy Stickers!">
