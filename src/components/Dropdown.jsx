a85a9d8a192b96d06e6a7281e0cc10219356fb2c
Rajnish <rshonkhia65@gmail.com>
2025-10-10 00:28:00 +0530

    dark theme added

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -8,14 +8,16 @@ export default function Dropdown({
 }) {
   return (
     <div className="flex items-center gap-2 justify-end pr-12 flex-1 font-primary">
-      <label className="text-md font-semibold text-primary">{label}</label>
+      <label className="text-md font-semibold text-primary dark:text-light">
+        {label}
+      </label>
       <select
-        className="px-2 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-900"
+        className="px-2 py-2 text-base border rounded-md transition border-primary dark:border-light focus:ring focus:ring-dark focus:outline-none text-gray-900 dark:text-gray-100"
         value={selectedValue}
         onChange={(event) => handleSort(event.target.value)}
       >
         {options.map((optionVal, index) => (
-          <option key={index} value={optionVal}>
+          <option key={index} value={optionVal} className="dark:bg-darkbg">
             {optionVal}
           </option>
         ))}
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,10 +3,12 @@ import React from "react";
 export default function SearchBox({ label, placeholder, value, handleChange }) {
   return (
     <div className="flex items-center gap-3 pl-4 flex-1 font-primary">
-      <label className="text-md font-semibold text-primary">{label}</label>
+      <label className="text-md font-semibold text-primary dark:text-light">
+        {label}
+      </label>
       <input
         type="text"
-        className="px-2 py-1 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
+        className="px-2 py-1 text-base border rounded-md transition border-primary dark:border-light focus:ring focus:ring-dark focus:outline-none text-gray-800 dark:text-gray-100"
         placeholder={placeholder}
         value={value}
         onChange={(event) => handleChange(event.target.value)}
